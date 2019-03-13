f0d9a3b294a0a30a056c1bd5433af33a7cead010
Nikita Prudnik <prudniknick@gmail.com>
2019-03-13 18:28:24 +0300

    Carousel

---
+++ src/components/carousel/arrows.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+
+// export const RightArrow = (props) => {
+//   return (
+//     <div className="nextArrow" onClick={props.goToNextSlide}>
+//       <i className="fa fa-arrow-right fa-2x" aria-hidden="true"></i>
+//     </div>
+//   );
+// }
+
+// export const LeftArrow = (props) => {
+//   return (
+//     <div className="backArrow" onClick={props.goToPrevSlide}>
+//       <i className="fa fa-arrow-left fa-2x" aria-hidden="true"></i>
+//     </div>
+//   );
+// }
